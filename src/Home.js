18a34f126b81a75b9a35572b3eb4fd6b528f76cc
premaseem <premaseem.webmaster@gmail.com>
2018-09-19 12:04:12 -0500

    added routes and nav without makeup

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+ 
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>HELLO</h2>
+        <p>This is my Home page, search page </p>
+ 
+        <p>You are welcome to my home</p>
+      </div>
+    );
+  }
+}
+ 
+export default Home;
